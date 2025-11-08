f21d4f618ff9ac0703d3ba8286702fd65e04135c
GabrielDyck <gabrielfernandodyck@gmail.com>
2025-11-08 20:41:11 -0300

    remove useless var:

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from langchain import hub
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain_classic import hub
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     llm = ChatOpenAI()
 
     query = "what is Pinecone in machine learning?"
-    chain = PromptTemplate.from_template(template=query) | llm
+    #chain = PromptTemplate.from_template(template=query) | llm
     # result = chain.invoke(input={})
     # print(result.content)
 
